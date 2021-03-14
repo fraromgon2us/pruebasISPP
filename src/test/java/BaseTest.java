bcd1ebc3670d2e1a89e771c099cf3aa604e2a653
Francisco Javier Romero González-Regueral <56021961+fraromgon2us@users.noreply.github.com>
2021-03-14 09:50:07 +0100

    travis5

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,16 @@
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class BaseTest {
+
+    @Test
+    public void testConcatenate() {
+
+        assertEquals("onetwo", "onetwo");
+
+    }
+}
+
